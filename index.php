ad1bf3e1854fecf8baf9e012da35e74c0c092592
Deepak verma <deepcloud.28@gmail.com>
2023-09-23 17:51:53 +0530

    php-e-commerce

--- index.php
+++ index.php
@@ -5,7 +5,7 @@
         <meta http-equiv="X-UA-Compatible" content="IE=edge">
         <meta name="viewport" content="width=device-width, initial-scale=1">
 
-        <title>DeepCloud E-Commerce</title>
+        <title>Rapidcode E-Commerce</title>
 
         <!-- Favicon -->
         <link rel="icon" href="img/favicon.png" type="image/png" />
@@ -54,7 +54,9 @@
                             <span class="icon-bar"></span>
                             <span class="icon-bar"></span>
                             </button>
+                            <a class="navbar-brand" href="index.html">
 
+                            </a>
                         </div>
                     </div>
 
@@ -86,7 +88,7 @@
         <section class="slider_area row m0">
             <div class="slider_inner">
                 <div class="camera_caption">
-                    <h2 class="wow fadeInUp animated">Make shopping easy</h2>
+                    <h2 class="wow fadeInUp animated">Make Your Shopping Easy</h2>
                     <h5 class="wow fadeIn animated" data-wow-delay="0.3s">Find everything accordingly</h5>
                     <a class="learn_mor wow fadeInU" data-wow-delay="0.6s" href="#product-list">Show Now!</a>
                 </div>
@@ -100,13 +102,8 @@
             </div>
             <div class="row it_works">
               <?php
-//                         $dbconfig = parse_ini_file(".env");
-                        $host = getenv('DB_HOST');//$dbconfig["DB_HOST"];
-                        $user = getenv('DB_USERNAME'); //$dbconfig["DB_USERNAME"];
-                        $pass = getenv('DB_PASSWORD'); //$dbconfig["DB_PASSWORD"];
-                        $db = getenv('DB_DATABASE'); //$dbconfig["DB_DATABASE"];
 
-                        $link = mysqli_connect($host,$user,$pass,$db);
+                        $link = mysqli_connect('db', 'rapidcode', 'rapidcode123', 'ecomdb');
 
                         if ($link) {
                         $res = mysqli_query($link, "select * from products;");
@@ -156,7 +153,7 @@
 
 
                 <div class="copy_right_area">
-                    <h4 class="copy_right">© Copyright 2021 DeepCloud Ecommerce | All Rights Reserved</h4>
+                    <h4 class="copy_right">© Copyright 2019 Rapidcode Ecommerce | All Rights Reserved</h4>
                 </div>
             </div>
         </footer>
